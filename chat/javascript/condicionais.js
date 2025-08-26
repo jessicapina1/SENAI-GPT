850d6135c7e1328529d65752888cb08f99600466
jessicapina1 <jessicapinalima@gmail.com>
2025-08-25 21:53:59 -0300

    Exercicios de JavaScript

---
+++ chat/javascript/condicionais.js
@@ -0,0 +1,111 @@
+//Exercicio 1
+
+// let idade = prompt("Qual a sua idade?");
+
+// if (idade >= 18) {alert ("Voce e maior de idade!")
+
+
+// }
+// else { alert ("Va pra Super Nanny")
+
+
+// } 
+
+
+// Exercicio 2
+
+// let numero = prompt ("Insira um numero");
+
+// if (numero >= 0) { alert ("Numero positivo")
+
+// }
+
+// else { alert ("Numero negativo")}
+
+
+//Exercicio 3
+
+// let nota = prompt ("Digite a nota do aluno")
+
+// if (nota >= 60) { alert ("Aprovado")}
+// else { alert ("Reprovado")}
+
+//Exercicio 4
+
+// let numero = prompt ("Digite um numero")
+
+// if (numero >0) { 
+//     alert ("numero positivo")
+
+
+// } else { 
+    
+
+//     if (numero == 0) {
+//         alert ("nulo")
+//     } else { alert ("numero negativo")}
+// }
+
+//Exercicio 5
+
+// let idade = prompt("Qual a sua idade?")
+// if (idade >= 18) {alert ("voce e um Adulto")}
+// if (idade <= 12 && idade >= 0) {alert ("voce e uma Crianca")}
+// if (idade >=13 && idade <= 17) {alert ("voce um adolescente")}
+// if (idade <0) {alert ("idade invalida")}
+
+//Exercicio 6
+
+// let numero = prompt ("Insira um numero")
+
+// if (numero %2==0) {alert ("O numero e par")
+
+// } else { alert ("O numero e impar")}
+
+
+//Exercicio Inter 1
+
+// let num = prompt ("Digite um numero");
+// let seg = prompt ("Digite outro numero");
+// let operacao = prompt ("Escolha a operacao a ser realizada");
+
+
+// if (operacao="+") {alert (num+seg)}
+
+// if (operacao ="-") {alert (num-seg)}
+
+// if (operacao ="/") {alert (num/seg)}
+
+// if (operacao ="*") {alert (num*seg)}
+
+
+//Exercicio Inter 2
+
+// let first = prompt ("Primeiro numero")
+// let second = prompt ("Segundo numero")
+// let third = prompt ("Terceiro numero")
+
+// if (first > second && first > third) { alert (first)}
+
+// if (second > third && second > first) {alert (second)}
+
+// if (third > first && third > second) {alert (third)}
+
+
+//Exercicio Inter 3
+
+// let valor = prompt ("Valor da compra")
+
+// if (valor>=100) {alert (valor*0.9)
+
+// } else {alert (valor)}
+
+
+//Exercicio Inter 4
+
+// let usuario = prompt ("Digite o usuario")
+// let senha = prompt ("Insira a senha")
+
+// if (usuario == "admin" && senha == 1234) {alert ("Login bem sucedido")
+
+// } else {alert ("Acesso negado")}
